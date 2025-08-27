cf243332549a1508974dd5ddc891ba073aa5c16e
teoodbs <h.teodorobs@gmail.com>
2025-08-27 02:57:53 -0300

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // gera site estático em ./out para o GitHub Pages
+  output: "export",
+
+  // se um dia usar <Image />, evita otimização no build estático do Pages
+  images: { unoptimized: true },
+
+  // como seu repositório NÃO é teooodbs.github.io, precisa do basePath
+  basePath: "/Flor-de-Mandacaru",
+  assetPrefix: "/Flor-de-Mandacaru/",
+
+  // ajuda o Pages a servir /pasta/index.html
+  trailingSlash: true,
 };
 
 export default nextConfig;
